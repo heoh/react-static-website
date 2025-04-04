e61635c7864a76b36c82419a87c4f2e369aadf31
heoh <h0h6h2h5@gmail.com>
2025-04-05 01:58:33 +0900

    Install tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: `/${path.basename(path.resolve())}/`,
 })
