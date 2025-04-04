0ab421409756fbd67b886bb9c4de6ee2ebb476b9
heoh <h0h6h2h5@gmail.com>
2025-04-05 02:37:13 +0900

    Add about link

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { NavLink } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <NavLink to="/about">About</NavLink>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
