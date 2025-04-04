7d9d651ad83102ca65425094123265f579937f01
heoh <h0h6h2h5@gmail.com>
2025-04-05 02:03:48 +0900

    Add about page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import About from './About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route index element={<App />} />
+        <Route path='about' element={<About />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
